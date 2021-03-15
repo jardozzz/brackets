90d730eafb28617c8788739be1da0dfe2fa8930c
Jardozzz <jardozzz@gmail.com>
2021-03-15 16:52:54 +0300

    123

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let a=bracketsConfig.map(e=>e.join(""));
+ while (true){
+   let b=str
+ for (i=0;i<a.length;i=i+1){
+ str=str.replace(a[i],"")
 }
+if (b==str && b!=""){
+  return false;
+  break;
+}
+else if(b==str && b=="") {
+ return true;
+  break;
+}
+
+}
+}
+
